cd44d231436f9e70af342d16a0e4e14e115ce6e0
Lucas B. Amaral <l.amaralof12@gmail.com>
2025-09-27 19:36:26 -0300

    att

--- fooderama/app/db_config.py
+++ fooderama/app/db_config.py
@@ -1,10 +1,12 @@
 import mysql.connector
+import os
 
 def get_db_connection():
     connection = mysql.connector.connect(
-        host='127.0.0.1',
-        user='root',
-        password='angryjam123',
-        database='fooderama'
+        host=os.getenv('DB_HOST', '127.0.0.1'),
+        user=os.getenv('DB_USER', 'root'),
+        password=os.getenv('DB_PASSWORD', 'root'),
+        database=os.getenv('DB_NAME', 'fooderama'),
+        port=int(os.getenv('DB_PORT', 3306))
     )
     return connection
