04c6a7191e3d96db2072171d61574448de019fec
Lucas B. Amaral <l.amaralof12@gmail.com>
2025-09-28 23:16:16 -0300

    atualização quase final

--- fooderama/BANDO DE DADOS/05-add-observacoes-pedido.sql
+++ fooderama/BANDO DE DADOS/05-add-observacoes-pedido.sql
@@ -1,8 +1,10 @@
 -- Script para adicionar campo de observações na tabela pedido
 -- Execute este script no banco de dados para adicionar o campo observações
+DELIMITER //
 
 ALTER TABLE `pedido` 
 ADD COLUMN `observacoes` TEXT NULL AFTER `status`;
 
 -- Verificar se a coluna foi adicionada corretamente
 DESCRIBE `pedido`;
+DELIMITER ;
